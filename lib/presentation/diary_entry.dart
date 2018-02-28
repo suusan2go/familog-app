ed808899f0a91a2888ed103ea2449117e35d5297
suusan2go <ksuzuki180@gmail.com>
2018-02-28 23:51:04 +0900

    Add Dots for Page View Image Slider

--- lib/presentation/diary_entry.dart
+++ lib/presentation/diary_entry.dart
@@ -1,5 +1,72 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'dart:math';
+
+
+class DotsIndicator extends AnimatedWidget {
+  DotsIndicator({
+    this.controller,
+    this.itemCount,
+    this.onPageSelected,
+    this.color: Colors.white,
+  }) : super(listenable: controller);
+
+  /// The PageController that this DotsIndicator is representing.
+  final PageController controller;
+
+  /// The number of items managed by the PageController
+  final int itemCount;
+
+  /// Called when a dot is tapped
+  final ValueChanged<int> onPageSelected;
+
+  /// The color of the dots.
+  ///
+  /// Defaults to `Colors.white`.
+  final Color color;
+
+  // The base size of the dots
+  static const double _kDotSize = 4.0;
+
+  // The increase in the size of the selected dot
+  static const double _kMaxZoom = 2.0;
+
+  // The distance between the center of each dot
+  static const double _kDotSpacing = 25.0;
+
+  Widget _buildDot(int index) {
+    double selectedness = Curves.easeOut.transform(
+      max(
+        0.0,
+        1.0 - ((controller.page ?? controller.initialPage) - index).abs(),
+      ),
+    );
+    double zoom = 1.0 + (_kMaxZoom - 1.0) * selectedness;
+    return new Container(
+      width: _kDotSpacing,
+      child: new Center(
+        child: new Material(
+          color: color,
+          type: MaterialType.circle,
+          child: new Container(
+            width: _kDotSize * zoom,
+            height: _kDotSize * zoom,
+            child: new InkWell(
+              onTap: () => onPageSelected(index),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget build(BuildContext context) {
+    return new Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: new List<Widget>.generate(itemCount, _buildDot),
+    );
+  }
+}
 
 class DiaryEntry extends StatefulWidget {
   DiaryEntry(String title): title = title;
@@ -16,6 +83,8 @@ class _DiaryEntryState extends State<DiaryEntry> {
 
   PageController pageController;
   int page = 0;
+  static const _kDuration = const Duration(milliseconds: 300);
+  static const _kCurve = Curves.ease;
 
   @override
   void initState() {
@@ -50,14 +119,39 @@ class _DiaryEntryState extends State<DiaryEntry> {
               new Container(
                   height: 200.0,
                   color: Colors.black12,
-                  child: new PageView(
-                      children: [
-                        new Image.network(sampleUri, height: 100.0, width: 100.0, fit: BoxFit.contain),
-                        new Container(color: Colors.blue),
-                        new Container(color: Colors.grey)
-                      ],
-                      controller: pageController,
-                      onPageChanged: onPageChanged
+                  child:  new Stack(
+                    children: <Widget>[
+                      new PageView(
+                          children: [
+                            new Image.network(sampleUri, height: 100.0, width: 100.0, fit: BoxFit.contain),
+                            new Container(color: Colors.blue),
+                            new Container(color: Colors.grey)
+                          ],
+                          controller: pageController,
+                          onPageChanged: onPageChanged
+                      ),
+                      new Positioned(
+                        bottom: 0.0,
+                        left: 0.0,
+                        right: 0.0,
+                        child: new Container(
+                          padding: const EdgeInsets.all(10.0),
+                          child: new Center(
+                            child: new DotsIndicator(
+                              controller: pageController,
+                              itemCount: 3,
+                              onPageSelected: (int page) {
+                                pageController.animateToPage(
+                                  page,
+                                  duration: _kDuration,
+                                  curve: _kCurve,
+                                );
+                              },
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   )
               ),
               new Container(
