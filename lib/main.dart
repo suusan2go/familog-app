667739c8f91097868ca3b92197445689c9f56407
suusan2go <ksuzuki180@gmail.com>
2018-03-10 01:17:42 +0900

    change data source to dummy resource

--- lib/main.dart
+++ lib/main.dart
@@ -50,18 +50,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -78,7 +66,7 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: new Text(widget.title),
       ),
-      body: new Home(_counter, _incrementCounter),
+      body: new Home(),
       floatingActionButton: new FloatingActionButton(
         onPressed: (){
           Navigator.of(context).push(new MaterialPageRoute(
--- lib/presentation/home.dart
+++ lib/presentation/home.dart
@@ -1,18 +1,34 @@
+import 'package:familog/domain/diary_entry.dart';
+import 'package:familog/domain/diary_entry_repository.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:familog/presentation/diary_entry_detail.dart';
 
 typedef increment = void Function();
 
+class Home extends StatefulWidget {
 
-class Home extends StatelessWidget {
-  Home(int counter, increment incrementCounter):
-    _incrementCounter = incrementCounter,
-    _counter = counter;
+  @override
+  State<StatefulWidget> createState() {
+    return new _HomeState();
+  }
+}
 
-  final increment _incrementCounter;
-  final int _counter;
-  
+class _HomeState extends State<Home> {
+
+  DiaryEntryRepository repository;
+  List<DiaryEntry> _entries;
+
+  @override
+  void initState() {
+    super.initState();
+    repository =new DiaryEntryRepository();
+    var entries = repository.findAll();
+    setState(() {
+      this._entries = entries;
+    });
+  }
+
   Widget _buildNotLoggedIn(BuildContext context) {
     return new Center(
       // Center is a layout widget. It takes a single child and positions it
@@ -33,20 +49,8 @@ class Home extends StatelessWidget {
         // horizontal).
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          new Container(
-            padding: const EdgeInsets.all(20.0),
-            child: new RaisedButton(onPressed: _incrementCounter, child: new Text("家族を招待する")),
-          ),
-          new Container(
-            padding: const EdgeInsets.all(20.0),
-            child: new RaisedButton(onPressed: _incrementCounter, child: new Text("日記を始める")),
-          ),
           new Text(
             'You have pushed the button this many times:',
-          ),
-          new Text(
-            '$_counter',
-            style: Theme.of(context).textTheme.display1,
           ),
         ],
       ),
@@ -58,45 +62,45 @@ class Home extends StatelessWidget {
     if(false) return _buildNotLoggedIn(context);
     String sampleUri = "http://benesse.jp/kosodate/201709/img/KJ_20170908_02.jpg";// "https://www.photolibrary.jp/mhd6/img222/450-20110922175418165134.jpg";
     return new ListView(
-      children: <Widget>[
-        new Card(
-          child: new InkWell(
-            onTap: () {
-              Navigator.of(context).push(new MaterialPageRoute(
-                builder: (context) => new DiaryEntryDetail("日記ですよ！", 1),
-              ));
-            },
-            child: new Row(
-              children: <Widget>[
-                new Image.network(sampleUri, height: 100.0, width: 100.0, fit: BoxFit.cover),
-                new Expanded(
-                    child:  new Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        new Container(
-                          child: new Text('2018/2/11 すーさんの日記☺', softWrap: true, style: new TextStyle(
-                            fontWeight: FontWeight.w300,
-                            fontSize: 16.0
-                          ),),
-                          padding: const EdgeInsets.only(left: 10.0, right: 10.0),
-                        ),
-                        new Container(
-                          child: new Text('今日はしほはじーじ・ばーばの家にいって楽しそうだった。このテキストはダミーですこのテキストはダミーですこのテキストはダミーです今日はしほはじーじ・ばーばの家にいって楽しそうだった。このテキストはダミーですこのテキストはダミーですこのテキストはダミーです今日はしほはじーじ・ばーばの家にいって楽しそうだった。このテキストはダミーですこのテキストはダミーですこのテキストはダミーです',
-                            softWrap: true,
-                            overflow: TextOverflow.ellipsis,
-                            maxLines: 3,
-                            style: new TextStyle(
-                              fontSize: 14.0,
-                              color: Colors.black87
-                            ),
+        children: this._entries.map( (DiaryEntry entry) {
+          return new Card(
+            child: new InkWell(
+              onTap: () {
+                Navigator.of(context).push(new MaterialPageRoute(
+                  builder: (context) => new DiaryEntryDetail("日記ですよ！", 1),
+                ));
+              },
+              child: new Row(
+                children: <Widget>[
+                  new Image.network(sampleUri, height: 100.0, width: 100.0, fit: BoxFit.cover),
+                  new Expanded(
+                      child:  new Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          new Container(
+                            child: new Text('2018/2/11 すーさんの日記☺', softWrap: true, style: new TextStyle(
+                                fontWeight: FontWeight.w300,
+                                fontSize: 16.0
+                            ),),
+                            padding: const EdgeInsets.only(left: 10.0, right: 10.0),
                           ),
-                          padding: const EdgeInsets.only(left: 10.0, right: 10.0),
-                        )
-                      ],
-                    )
-                )
-              ],
-            ),
+                          new Container(
+                            child: new Text('今日はしほはじーじ・ばーばの家にいって楽しそうだった。このテキストはダミーですこのテキストはダミーですこのテキストはダミーです今日はしほはじーじ・ばーばの家にいって楽しそうだった。このテキストはダミーですこのテキストはダミーですこのテキストはダミーです今日はしほはじーじ・ばーばの家にいって楽しそうだった。このテキストはダミーですこのテキストはダミーですこのテキストはダミーです',
+                              softWrap: true,
+                              overflow: TextOverflow.ellipsis,
+                              maxLines: 3,
+                              style: new TextStyle(
+                                  fontSize: 14.0,
+                                  color: Colors.black87
+                              ),
+                            ),
+                            padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+                          )
+                        ],
+                      )
+                  )
+                ],
+              ),
 //            child: <Widget>[
 //              new ListTile(
 //                leading: new CircleAvatar(
@@ -107,21 +111,9 @@ class Home extends StatelessWidget {
 //                isThreeLine: true,
 //              ),
 //            ],
-          ),
-        ),
-        new Card(
-          child: new Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              const ListTile(
-                title: const Text('2018/2/11 すーさんの日記☺'),
-                subtitle: const Text('今日はしほはじーじ・ばーばの家にいって楽しそうだった。このテキストはダミーですこのテキストはダミーですこのテキストはダミーです'),
-                isThreeLine: true,
-              ),
-            ],
-          ),
-        )
-      ],
+            ),
+          );
+        }).toList()
     );
   }
 }
