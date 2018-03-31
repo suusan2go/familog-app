683df6bd1ae7ec78715b46f7754da7181a51c479
suusan2go <ksuzuki180@gmail.com>
2018-04-01 00:39:09 +0900

    add buttons to form

--- lib/presentation/diary_entry_form.dart
+++ lib/presentation/diary_entry_form.dart
@@ -89,124 +89,129 @@ class _DiaryEntryFormState extends State<DiaryEntryForm> {
         // the App.build method, and use it to set our appbar title.
         title: new Text("日記を投稿"),
       ),
-      body: new Container(
-        color: Colors.grey[100],
-        child: new Column(
-          children: <Widget>[
-            new ListTile(
-              title: new Text("本文"),
-            ),
-            new TextFormField(
-              decoration: const InputDecoration(
-                  hintText: '本文を入力してください',
+      body: new ListView(
+        children: <Widget>[
+          new ListTile(
+            title: new Text("本文"),
+          ),
+          new TextFormField(
+            decoration: const InputDecoration(
+                hintText: '本文を入力してください',
                 filled: true,
                 fillColor: Colors.white
+            ),
+            maxLines: 10,
+          ),
+          new ListTile(
+            title: new Text("今日の気分"),
+          ),
+          new ButtonBar(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              new RaisedButton.icon(
+                icon: const Text('😁'),
+                label: const Text('楽しい'),
+                onPressed: _emoji == "happy" ? null : this.onPressedHappy,
+                color: Colors.white,
               ),
-              maxLines: 10,
-            ),
-            new ListTile(
-              title: new Text("今日の気分"),
-            ),
-            new ButtonBar(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                new RaisedButton.icon(
-                  icon: const Text('😁'),
-                  label: const Text('楽しい'),
-                  onPressed: _emoji == "happy" ? null : this.onPressedHappy,
-                  color: Colors.white,
-                ),
-                new RaisedButton.icon(
-                  color: Colors.white,
-                  icon: const Text('😩'),
-                  label: const Text('疲れた'),
-                  onPressed: _emoji == "tired" ? null : this.onPressedTired,
-                ),
-                new RaisedButton.icon(
-                  color: Colors.white,
-                  icon: const Text('😥'),
-                  label: const Text('悲しい'),
-                  onPressed: _emoji == "sad" ? null : this.onPressedSad,
-                ),
-              ],
-            ),
-            new ListTile(
-              title: new Text("写真"),
-            ),
-            new Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                new Expanded(
-                    child: new Container(
-                        color: Colors.white,
-                        padding: new EdgeInsets.only(left: 10.0, right: 10.0),
-                        height: 100.0,
-                        child: _imageFile1 == null ? new IconButton(
-                            icon: new Icon(Icons.image, size: 80.0),
-                            tooltip: 'Increase volume by 10%',
-                            onPressed: getImage1
-                        ): new Stack(
-                          fit: StackFit.expand,
-                          children: <Widget>[
-                            Image.file(_imageFile1, fit: BoxFit.contain),
-                            new IconButton(
-                              alignment: new Alignment(1.5, -1.0),
-                              icon: new Icon(Icons.close, color: Colors.grey),
-                              onPressed: deleteImage1,
-                            )
-                          ],
-                        )
-                    )
-                ),
-                new Expanded(
-                    child: new Container(
-                        color: Colors.white,
-                        padding: new EdgeInsets.only(left: 10.0, right: 10.0),
-                        height: 100.0,
-                        child: _imageFile2 == null ? new IconButton(
-                            icon: new Icon(Icons.image, size: 80.0),
-                            tooltip: 'Increase volume by 10%',
-                            onPressed: getImage2
-                        ): new Stack(
-                          fit: StackFit.expand,
-                          children: <Widget>[
-                            Image.file(_imageFile2, fit: BoxFit.contain),
-                            new IconButton(
-                              alignment: new Alignment(1.5, -1.0),
-                              icon: new Icon(Icons.close, color: Colors.grey),
-                              onPressed: deleteImage2,
-                            )
-                          ],
-                        )
-                    )
-                ),
-                new Expanded(
-                    child: new Container(
-                        color: Colors.white,
-                        padding: new EdgeInsets.only(left: 10.0, right: 10.0),
-                        height: 100.0,
-                        child: _imageFile3 == null ? new IconButton(
-                            icon: new Icon(Icons.image, size: 80.0),
-                            tooltip: 'Increase volume by 10%',
-                            onPressed: getImage3
-                        ): new Stack(
-                          fit: StackFit.expand,
-                          children: <Widget>[
-                            Image.file(_imageFile3, fit: BoxFit.contain),
-                            new IconButton(
-                              alignment: new Alignment(1.5, -1.0),
-                              icon: new Icon(Icons.close, color: Colors.grey),
-                              onPressed: deleteImage3,
-                            )
-                          ],
-                        )
-                    )
-                ),
-              ],
-            )
-          ],
-        ),
+              new RaisedButton.icon(
+                color: Colors.white,
+                icon: const Text('😩'),
+                label: const Text('疲れた'),
+                onPressed: _emoji == "tired" ? null : this.onPressedTired,
+              ),
+              new RaisedButton.icon(
+                color: Colors.white,
+                icon: const Text('😥'),
+                label: const Text('悲しい'),
+                onPressed: _emoji == "sad" ? null : this.onPressedSad,
+              ),
+            ],
+          ),
+          new ListTile(
+            title: new Text("写真"),
+          ),
+          new Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              new Expanded(
+                  child: new Container(
+                      color: Colors.white,
+                      padding: new EdgeInsets.only(left: 10.0, right: 10.0),
+                      height: 100.0,
+                      child: _imageFile1 == null ? new IconButton(
+                          icon: new Icon(Icons.image, size: 80.0),
+                          tooltip: 'Increase volume by 10%',
+                          onPressed: getImage1
+                      ): new Stack(
+                        fit: StackFit.expand,
+                        children: <Widget>[
+                          Image.file(_imageFile1, fit: BoxFit.contain),
+                          new IconButton(
+                            alignment: new Alignment(1.5, -1.0),
+                            icon: new Icon(Icons.close, color: Colors.grey),
+                            onPressed: deleteImage1,
+                          )
+                        ],
+                      )
+                  )
+              ),
+              new Expanded(
+                  child: new Container(
+                      color: Colors.white,
+                      padding: new EdgeInsets.only(left: 10.0, right: 10.0),
+                      height: 100.0,
+                      child: _imageFile2 == null ? new IconButton(
+                          icon: new Icon(Icons.image, size: 80.0),
+                          tooltip: 'Increase volume by 10%',
+                          onPressed: getImage2
+                      ): new Stack(
+                        fit: StackFit.expand,
+                        children: <Widget>[
+                          Image.file(_imageFile2, fit: BoxFit.contain),
+                          new IconButton(
+                            alignment: new Alignment(1.5, -1.0),
+                            icon: new Icon(Icons.close, color: Colors.grey),
+                            onPressed: deleteImage2,
+                          )
+                        ],
+                      )
+                  )
+              ),
+              new Expanded(
+                  child: new Container(
+                      color: Colors.white,
+                      padding: new EdgeInsets.only(left: 10.0, right: 10.0),
+                      height: 100.0,
+                      child: _imageFile3 == null ? new IconButton(
+                          icon: new Icon(Icons.image, size: 80.0),
+                          tooltip: 'Increase volume by 10%',
+                          onPressed: getImage3
+                      ): new Stack(
+                        fit: StackFit.expand,
+                        children: <Widget>[
+                          Image.file(_imageFile3, fit: BoxFit.contain),
+                          new IconButton(
+                            alignment: new Alignment(1.5, -1.0),
+                            icon: new Icon(Icons.close, color: Colors.grey),
+                            onPressed: deleteImage3,
+                          )
+                        ],
+                      )
+                  )
+              ),
+            ],
+          ),
+          new Container(
+            padding: const EdgeInsets.all(20.0),
+            child: new RaisedButton(onPressed: (){}, child: new Text("下書き")),
+          ),
+          new Container(
+            padding: const EdgeInsets.only(left: 20.0, top: 0.0, right: 20.0, bottom: 20.0),
+            child: new RaisedButton(onPressed: (){}, child: new Text("公開"), color: Theme.of(context).accentColor,textColor: Colors.white,),
+          ),
+        ],
       ),
     );
   }
