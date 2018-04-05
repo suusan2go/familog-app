418aa2df2de74d1d758d6740a8b99cfffc5d955e
suusan2go <ksuzuki180@gmail.com>
2018-04-05 20:36:24 +0900

    add upload form

--- lib/presentation/diary_entry_form.dart
+++ lib/presentation/diary_entry_form.dart
@@ -1,10 +1,15 @@
+import 'dart:async';
 import 'dart:io';
+import 'dart:math';
 
 import 'package:familog/domain/diary.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:path/path.dart';
 
 class DiaryEntryForm extends StatefulWidget {
   const DiaryEntryForm({ Key key, this.currentDiary }): super(key: key);
@@ -17,85 +22,109 @@ class DiaryEntryForm extends StatefulWidget {
   }
 }
 
+final auth = FirebaseAuth.instance;
+
 class _DiaryEntryFormState extends State<DiaryEntryForm> {
+  final scaffoldKey = new GlobalKey<ScaffoldState>();
+
   String _body;
   String _emoji;
   File _imageFile1;
   File _imageFile2;
   File _imageFile3;
 
-  void onChangeBody(String body) {
+  void _onChangeBody(String body) {
     setState(() {
       this._body = body;
     });
   }
 
-  void onPressedSad() {
+  void _onPressedSad() {
     setState(() {
       this._emoji = "sad";
     });
   }
 
-  void onPressedTired() {
+  void _onPressedTired() {
     setState(() {
       this._emoji = "tired";
     });
   }
 
-  void onPressedHappy() {
+  void _onPressedHappy() {
     setState(() {
       this._emoji = "happy";
     });
   }
 
-  void getImage1() async {
+  void _getImage1() async {
     var _fileName = await ImagePicker.pickImage();
     setState(() {
       _imageFile1 = _fileName;
     });
   }
 
-  void getImage2() async {
+  void _getImage2() async {
     var _fileName = await ImagePicker.pickImage();
     setState(() {
       _imageFile2 = _fileName;
     });
   }
 
-  void getImage3() async {
+  void _getImage3() async {
     var _fileName = await ImagePicker.pickImage();
     setState(() {
       _imageFile3 = _fileName;
     });
   }
 
-  void deleteImage1() async {
+  void _deleteImage1() async {
     setState(() {
       _imageFile1 = null;
     });
   }
 
-  void deleteImage2() async {
+  void _deleteImage2() async {
     setState(() {
       _imageFile2 = null;
     });
   }
 
-  void deleteImage3() async {
+  void _deleteImage3() async {
     setState(() {
       _imageFile3 = null;
     });
   }
 
-  void _handlePublish() {
+  Future<String> _uploadFile(File file) async {
+    if(file == null) return null;
+    final String rand = "${new Random().nextInt(10000)}";
+    final StorageReference ref = FirebaseStorage.instance.ref()
+        .child("/diaries/${widget.currentDiary.id}/diary_entry_images/$rand${extension(file.path)}");
+    final StorageUploadTask uploadTask = ref.put(file);
+    return (await uploadTask.future).downloadUrl.toString();
+  }
+
+  void _handlePublish(BuildContext context) async {
+    var currentUser = await auth.currentUser();
+    List<String> imageUrls = [];
+    imageUrls.add(await _uploadFile(_imageFile1));
+    imageUrls.add(await _uploadFile(_imageFile2));
+    imageUrls.add(await _uploadFile(_imageFile3));
     Firestore.instance.collection('diaries/${widget.currentDiary.id}/diary_entries').document()
-        .setData({ 'title': 'title', 'author': 'author' });
+        .setData({
+      'body': _body,
+      'emoji': _emoji,
+      'authorId': currentUser.uid,
+      'images': imageUrls.where((url) { return url != null; } ).toList()
+    });
     Navigator.of(context).pop();
   }
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
+      key: scaffoldKey,
       appBar: new AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
@@ -106,37 +135,39 @@ class _DiaryEntryFormState extends State<DiaryEntryForm> {
           new ListTile(
             title: new Text("本文"),
           ),
-          new TextFormField(
+          new TextField(
             decoration: const InputDecoration(
                 hintText: '本文を入力してください',
                 filled: true,
                 fillColor: Colors.white
             ),
             maxLines: 10,
+            onChanged: _onChangeBody,
           ),
           new ListTile(
             title: new Text("今日の気分"),
           ),
-          new ButtonBar(
-            mainAxisSize: MainAxisSize.min,
+          new Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
               new RaisedButton.icon(
                 icon: const Text('😁'),
                 label: const Text('楽しい'),
-                onPressed: _emoji == "happy" ? null : this.onPressedHappy,
+                onPressed: _emoji == "happy" ? null : this._onPressedHappy,
                 color: Colors.white,
               ),
               new RaisedButton.icon(
                 color: Colors.white,
                 icon: const Text('😩'),
                 label: const Text('疲れた'),
-                onPressed: _emoji == "tired" ? null : this.onPressedTired,
+                onPressed: _emoji == "tired" ? null : this._onPressedTired,
               ),
               new RaisedButton.icon(
                 color: Colors.white,
                 icon: const Text('😥'),
                 label: const Text('悲しい'),
-                onPressed: _emoji == "sad" ? null : this.onPressedSad,
+                onPressed: _emoji == "sad" ? null : this._onPressedSad,
               ),
             ],
           ),
@@ -155,7 +186,7 @@ class _DiaryEntryFormState extends State<DiaryEntryForm> {
                       child: _imageFile1 == null ? new IconButton(
                           icon: new Icon(Icons.image, size: 80.0),
                           tooltip: 'Increase volume by 10%',
-                          onPressed: getImage1
+                          onPressed: _getImage1
                       ): new Stack(
                         fit: StackFit.expand,
                         children: <Widget>[
@@ -163,7 +194,7 @@ class _DiaryEntryFormState extends State<DiaryEntryForm> {
                           new IconButton(
                             alignment: new Alignment(1.5, -1.0),
                             icon: new Icon(Icons.close, color: Colors.grey),
-                            onPressed: deleteImage1,
+                            onPressed: _deleteImage1,
                           )
                         ],
                       )
@@ -177,7 +208,7 @@ class _DiaryEntryFormState extends State<DiaryEntryForm> {
                       child: _imageFile2 == null ? new IconButton(
                           icon: new Icon(Icons.image, size: 80.0),
                           tooltip: 'Increase volume by 10%',
-                          onPressed: getImage2
+                          onPressed: _getImage2
                       ): new Stack(
                         fit: StackFit.expand,
                         children: <Widget>[
@@ -185,7 +216,7 @@ class _DiaryEntryFormState extends State<DiaryEntryForm> {
                           new IconButton(
                             alignment: new Alignment(1.5, -1.0),
                             icon: new Icon(Icons.close, color: Colors.grey),
-                            onPressed: deleteImage2,
+                            onPressed: _deleteImage2,
                           )
                         ],
                       )
@@ -199,7 +230,7 @@ class _DiaryEntryFormState extends State<DiaryEntryForm> {
                       child: _imageFile3 == null ? new IconButton(
                           icon: new Icon(Icons.image, size: 80.0),
                           tooltip: 'Increase volume by 10%',
-                          onPressed: getImage3
+                          onPressed: _getImage3
                       ): new Stack(
                         fit: StackFit.expand,
                         children: <Widget>[
@@ -207,7 +238,7 @@ class _DiaryEntryFormState extends State<DiaryEntryForm> {
                           new IconButton(
                             alignment: new Alignment(1.5, -1.0),
                             icon: new Icon(Icons.close, color: Colors.grey),
-                            onPressed: deleteImage3,
+                            onPressed: _deleteImage3,
                           )
                         ],
                       )
@@ -221,7 +252,7 @@ class _DiaryEntryFormState extends State<DiaryEntryForm> {
           ),
           new Container(
             padding: const EdgeInsets.only(left: 20.0, top: 0.0, right: 20.0, bottom: 20.0),
-            child: new RaisedButton(onPressed: _handlePublish, child: new Text("公開"), color: Theme.of(context).accentColor,textColor: Colors.white,),
+            child: new RaisedButton(onPressed: () {_handlePublish(context);}, child: new Text("公開"), color: Theme.of(context).accentColor,textColor: Colors.white,),
           ),
         ],
       ),
